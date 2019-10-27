c8ea74822063d6699d3282301c07138125997e23
Donovan <donovan@Donovan-MacBook-Pro.local>
2019-10-26 20:29:51 -0700

    added my solution for house robber

---
+++ algorithms/divide/index.js
@@ -0,0 +1,6 @@
+// Divide without using the divide symbol API
+// How would you create an API that took two numbers and divided them to get the result without using the divide symbol.
+
+// Two general ways to do this:
+// - Bit shifting - this tests the candidates ability to manipulate strings or knowledge of bit manipulation
+// - Binary search
